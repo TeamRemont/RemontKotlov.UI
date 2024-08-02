29939b73bd9062d29112ba544a13095a157617af
Abu_Programmiy <abdukholiq23@gmail.com>
2024-08-02 11:56:16 +0500

    main-page and navbar component genrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
