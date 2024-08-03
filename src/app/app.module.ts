22e2f80f5ddc6edd2412af55eb12b4591164cc13
Abu_Programmiy <abdukholiq23@gmail.com>
2024-08-03 20:25:23 +0500

    first section is end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainHeaderComponent } from './components/main-header/main-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainHeaderComponent
   ],
   imports: [
     BrowserModule,
